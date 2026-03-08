4bb1d6d17d6071cbf540a07f76a710c1a70f0786
SDF <47056965+gobeti@users.noreply.github.com>
2026-03-08 11:54:48 +0100

    Update 2026_02_13_171402_create_personal_access_tokens_table.php

--- database/migrations/2026_02_13_171402_create_personal_access_tokens_table.php
+++ database/migrations/2026_02_13_171402_create_personal_access_tokens_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 return new class extends Migration
 {
     /**
@@ -13,7 +11,7 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->uuidMorphs('tokenable');
             $table->text('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
@@ -22,7 +20,6 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      */
